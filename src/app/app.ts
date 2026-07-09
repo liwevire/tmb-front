38ec1a9c22cdba8e7ac4c9a898d90a604a6dd889
Premkumar <t.prem2040@gmail.com>
2026-07-09 08:15:02 +0530

    sidenav in progress 02 added all menu item

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { materialImports } from './util/globalImports';
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ...materialImports],
+  imports: [RouterOutlet, ...materialImports, MatTooltip],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('tmb-front');
   showFiller = false;
-  toggleSidenav() {
-    this.showFiller = true;
-  }
 }
